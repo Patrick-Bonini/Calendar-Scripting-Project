56ab8ac6b576663e48faead79815387702d8f267
Patrick-Bonini <patrick.bonini13@gmail.com>
2023-07-25 11:30:40 -0400

    Final commit, Done!

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+"""
+This program allows the user to upload their class schedule in text format and have it automatically uploaded 
+to their Google Calendar:
+-In weekly intervals
+-For the duration of a semester
+-Each class has a different colour
+
+This program only works for authorized users (as per google cloud console dashboard):
+patrick.bonini13@gmail.com
+"""
 import datetime as dt
 import os.path
+import re
 
+#text to google calendar
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -10,6 +22,9 @@ from googleapiclient.errors import HttpError
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 def main ():
+    """
+    If you're a first time user, signs in to your google account
+    """
     creds = None
 
     if os.path.exists('token.json'):
@@ -23,57 +38,49 @@ def main ():
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    
-    #Lists upcoming events
-    """ try:
-        service = build("calendar", "v3", credentials=creds)
+            token.write(creds.to_json()) 
 
-        now = dt.datetime.now().isoformat() + 'Z'
+    #Converts the text file into a list containing the different elements needed
+    textFromFile = open('schedule.txt', 'r')
+    text = textFromFile.read()
+    text = text.split('; ')
+    schedule = [i.split(', ') for i in text]
+    #Loops over the list
+    for i in range(len(schedule)):
+        #Assigns a different color code to each class
+        colors = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7]
+        currentColor = colors[i]
+        #Creates the calendar events
+        try:
+            service = build("calendar", "v3", credentials=creds)
 
-        event_result = service.events().list(calendarId='primary', timeMin=now, maxResults=10, singleEvents=True, orderBy="startTime").execute()
-        events = event_result.get('items', [])
+            event = {
+                'summary': schedule[i][0],       
+                'Description': 'Event details',
+                'colorId': currentColor,
+                'start': {
+                    'dateTime': schedule[i][1],
+                    'timeZone': 'America/Chicago'
+                },
+                'end': {
+                    'dateTime': schedule[i][2],
+                    'timeZone': 'America/Chicago'
+                },
+                'location': schedule[i][3],
+                'recurrence': [
+                    'RRULE:FREQ=WEEKLY;COUNT=13'
+                ],
+                'attendees': [
+                    {'email': 'patrick.bonini13@gmail.com'}
+                ]
+            }
 
-        if not events:
-            print("No upcoming events")
-            return
+            event = service.events().insert(calendarId='primary', body=event).execute()
 
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
-    except HttpError as error:
-        print("Error occurred:", error) """
-    
-    try:
-        service = build("calendar", "v3", credentials=creds)
+            print(f"Event created {event.get('htmlLink')}")
 
-        event = {
-            'summary': 'Event name',
-            'location': 'Event location',
-            'Description': 'Event details',
-            'colorId': 6,
-            'start': {
-                'dateTime': '2023-07-25T09:00:00-04:00',
-                'timeZone': 'Europe/Vienna'
-            },
-            'end': {
-                'dateTime': '2023-07-25T17:00:00-04:00',
-                'timeZone': 'Europe/Vienna'
-            },
-            'recurrence': [
-                'RRULE:FREQ=DAILY;COUNT=3'
-            ],
-            'attendees': [
-                {'email': 'patrick.bonini13@gmail.com'}
-            ]
-        }
-
-        event = service.events().insert(calendarId='primary', body=event).execute()
-
-        print(f"Event created {event.get('htmlLink')}")
-
-    except HttpError as error:
-        print("Error occurred:", error)
+        except HttpError as error:
+            print("Error occurred:", error)
 
 if __name__ == '__main__':
     main()
